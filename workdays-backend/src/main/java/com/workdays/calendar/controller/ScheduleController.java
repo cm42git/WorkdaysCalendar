3040acf4cd2d61c971f8f41f29e950be0b2834a0
cm42git <106208621+cm42git@users.noreply.github.com>
2022-10-28 12:07:57 -0400

    add swagger, controller returning correct info

--- workdays-backend/src/main/java/com/workdays/calendar/controller/ScheduleController.java
+++ workdays-backend/src/main/java/com/workdays/calendar/controller/ScheduleController.java
@@ -1,8 +1,40 @@
 package com.workdays.calendar.controller;
 
-import org.springframework.stereotype.Controller;
+import java.time.LocalDate;
+import java.util.List;
 
-@Controller
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.workdays.calendar.model.SchedulePattern;
+import com.workdays.calendar.model.StartEndDates;
+import com.workdays.calendar.service.ScheduleService;
+
+@RestController
+@CrossOrigin
 public class ScheduleController {
-    
+
+    @Autowired
+    ScheduleService scheduleService;
+
+    @GetMapping(path = "/scheduledates")
+    public List<StartEndDates> listStartEnd(
+            @RequestParam int daysOn,
+            @RequestParam int daysOff,
+            @RequestParam int daysOnAlt,
+            @RequestParam int daysOffAlt,
+            @RequestParam @DateTimeFormat(iso=ISO.DATE) LocalDate startLineOne,
+            @RequestParam @DateTimeFormat(iso = ISO.DATE) LocalDate begin,
+            @RequestParam int numMonths,
+            @RequestParam int lineNum) {
+        SchedulePattern sched = new SchedulePattern(daysOn, daysOff, daysOnAlt, daysOffAlt, startLineOne);
+        return scheduleService.listStartEnd(sched, begin, numMonths, lineNum);
+    }
+
 }
