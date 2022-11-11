a34b7c3a3515f2559c6bb38301ca0e928fe32c05
cm42git <106208621+cm42git@users.noreply.github.com>
2022-11-11 16:29:29 -0500

    january holidays

--- workdays-backend/src/main/java/com/workdays/calendar/service/RestHolidayService.java
+++ workdays-backend/src/main/java/com/workdays/calendar/service/RestHolidayService.java
@@ -1,5 +1,6 @@
 package com.workdays.calendar.service;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,7 +11,19 @@ public class RestHolidayService {
 
     public List<Holiday> getHolidays(LocalDate monthYear) {
         List<Holiday> holidays = new ArrayList<>();
+        holidays.add(getNewYearsDay(monthYear));
+        holidays.add(getMLKDay(monthYear));
         return holidays;
     }
+
+    private Holiday getNewYearsDay(LocalDate monthYear) {
+        return new Holiday("New Year's Day", LocalDate.of(monthYear.getYear(), 1, 1));
+    }
     
+    private Holiday getMLKDay(LocalDate monthYear) {
+        int weekdayFirstJanuary = LocalDate.of(monthYear.getYear(), 1, 1).getDayOfWeek().getValue();
+        // get the day of month for the 3rd Monday of January
+        int mlkDayOfMonth = ((8 - weekdayFirstJanuary) % 7) + 15;
+        return new Holiday("Martin Luther King Jr. Day", LocalDate.of(monthYear.getYear(), 1, mlkDayOfMonth));
+    }
 }
--- workdays-backend/src/test/java/com/workdays/calendar/service/RestHolidayServiceTest.java
+++ workdays-backend/src/test/java/com/workdays/calendar/service/RestHolidayServiceTest.java
@@ -18,8 +18,19 @@ public class RestHolidayServiceTest {
         List<Holiday> expectedHolidays = new ArrayList<>();
         expectedHolidays.add(new Holiday("New Year's Day", LocalDate.of(2022, 1, 1)));
         expectedHolidays.add(new Holiday("Martin Luther King Jr. Day", LocalDate.of(2022, 1, 17)));
-        
+
         List<Holiday> actualHolidays = holidayService.getHolidays(LocalDate.of(2022, 1, 1));
+
+        assertEquals(expectedHolidays, actualHolidays);
+    }
+
+    @Test
+    void getJanuary2024Holidays() {
+        List<Holiday> expectedHolidays = new ArrayList<>();
+        expectedHolidays.add(new Holiday("New Year's Day", LocalDate.of(2024, 1, 1)));
+        expectedHolidays.add(new Holiday("Martin Luther King Jr. Day", LocalDate.of(2024, 1, 15)));
+        
+        List<Holiday> actualHolidays = holidayService.getHolidays(LocalDate.of(2024, 1, 1));
 
         assertEquals(expectedHolidays, actualHolidays);
     }
