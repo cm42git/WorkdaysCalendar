deccb5382af2a5c2533a6bb93174351a526bc8b7
cm42git <106208621+cm42git@users.noreply.github.com>
2022-11-15 16:04:32 -0500

    day of month calculator

--- workdays-backend/src/main/java/com/workdays/calendar/service/RestHolidayService.java
+++ workdays-backend/src/main/java/com/workdays/calendar/service/RestHolidayService.java
@@ -2,6 +2,8 @@ package com.workdays.calendar.service;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAdjusters;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,7 +21,7 @@ public class RestHolidayService {
     private Holiday getNewYearsDay(LocalDate monthYear) {
         return new Holiday("New Year's Day", LocalDate.of(monthYear.getYear(), 1, 1));
     }
-    
+
     private Holiday getMLKDay(LocalDate monthYear) {
         int weekdayFirstJanuary = LocalDate.of(monthYear.getYear(), 1, 1).getDayOfWeek().getValue();
         // get the day of month for the 3rd Monday of January
@@ -31,24 +33,49 @@ public class RestHolidayService {
         int weekdayFirstFebruary = LocalDate.of(monthYear.getYear(), 2, 1).getDayOfWeek().getValue();
         // get the day of month for the 3rd Monday of February
         int presidentsDayOfMonth = 15 + ((8 - weekdayFirstFebruary) % 7);
-        return new Holiday("Presidents' Day", LocalDate.of(monthYear.getYear(), 1, presidentsDayOfMonth));
+        return new Holiday("Presidents' Day", LocalDate.of(monthYear.getYear(), 2, presidentsDayOfMonth));
     }
-    
+
     private Holiday getMemorialDay(LocalDate monthYear) {
-        int weekdayFirstFebruary = LocalDate.of(monthYear.getYear(), 5, 31).getDayOfWeek().getValue();
+        int weekdayLastMay = LocalDate.of(monthYear.getYear(), 5, 31).getDayOfWeek().getValue();
         // get the day of month for the last Monday of May
-        int memorialDayOfMonth = 31 - ((weekdayFirstFebruary - 1) % 7);
-        return new Holiday("Memorial Day", LocalDate.of(monthYear.getYear(), 1, memorialDayOfMonth));
+        int memorialDayOfMonth = 31 - ((weekdayLastMay - 1) % 7);
+        return new Holiday("Memorial Day", LocalDate.of(monthYear.getYear(), 5, memorialDayOfMonth));
     }
-    
+
     private Holiday getIndependenceDay(LocalDate monthYear) {
         return new Holiday("Independence Day", LocalDate.of(monthYear.getYear(), 7, 4));
     }
-    
-        private Holiday getLaborDay(LocalDate monthYear) {
-            int weekdayFirstSeptember = LocalDate.of(monthYear.getYear(), 2, 1).getDayOfWeek().getValue();
-            // get the day of month for the 1st Monday of September
-            int mlkDayOfMonth = 15 + ((8 - weekdayFirstSeptember) % 7);
-            return new Holiday("Presidents' Day", LocalDate.of(monthYear.getYear(), 1, mlkDayOfMonth));
+
+    private Holiday getLaborDay(LocalDate monthYear) {
+        int weekdayFirstSeptember = LocalDate.of(monthYear.getYear(), monthYear.getMonthValue(), 1).getDayOfWeek()
+                .getValue();
+        // get the day of month for the 1st Monday of September
+        int laborDayOfMonth = 1 + ((8 - weekdayFirstSeptember) % 7);
+        return new Holiday("Labor Day", LocalDate.of(monthYear.getYear(), 9, laborDayOfMonth));
+    }
+
+    /**
+     * @param monthYear
+     * @param weekday
+     * @param ordinal
+     */
+    public LocalDate getDayOfMonth(LocalDate monthYear, DayOfWeek weekday, int ordinal) {
+        // monthYear indicates the month and year to derive the day of month from
+        // weekday indicates which day of the week to return
+        // ordinal indicates which instance of the day of week to return (i.e. 1=first,
+        // 2=second, 3=third, 4=fourth, 5=last)
+        if (ordinal > 0 && ordinal < 5) {
+            DayOfWeek weekdayFirstOfMonth = monthYear.with(TemporalAdjusters.firstDayOfMonth()).getDayOfWeek();
+            int dayOfMonth = (7 * (ordinal - 1)) + ((7 - weekdayFirstOfMonth.getValue() + weekday.getValue()) % 7) + 1;
+            return LocalDate.of(monthYear.getYear(), monthYear.getMonthValue(), dayOfMonth);
+        } else if (ordinal == 5) {
+            DayOfWeek weekdayLastOfMonth = monthYear.with(TemporalAdjusters.lastDayOfMonth()).getDayOfWeek();
+            int dayOfMonth = monthYear.with(TemporalAdjusters.lastDayOfMonth()).getDayOfMonth()
+                    - (((weekdayLastOfMonth.getValue() - weekday.getValue() + 1) + 7) % 7);
+            return LocalDate.of(monthYear.getYear(), monthYear.getMonthValue(), dayOfMonth);
         }
+
+        return LocalDate.MIN;
+    }
 }
--- workdays-backend/src/test/java/com/workdays/calendar/service/RestHolidayServiceTest.java
+++ workdays-backend/src/test/java/com/workdays/calendar/service/RestHolidayServiceTest.java
@@ -2,6 +2,7 @@ package com.workdays.calendar.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -34,9 +35,97 @@ public class RestHolidayServiceTest {
 
         assertEquals(expectedHolidays, actualHolidays);
     }
+
+    // @Test
+    // void getLaborDay2022() {
+    // Holiday expected = new Holiday("Labor Day", LocalDate.of(2022, 9, 5));
+    // Holiday actual = holidayService.getLaborDay(LocalDate.of(2022, 9, 1));
+    // assertEquals(expected, actual);
+    // }
+
+    @Test
+    void testDaysOfMonthFunction() {
+        LocalDate expected = LocalDate.of(2022, 9, 5);
+        LocalDate actual = holidayService.getDayOfMonth(LocalDate.of(2022, 9, 1), DayOfWeek.MONDAY, 1);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testFirstWeekOfMonthSeptember2022() {
+        List<LocalDate> expectDaysOfSeptember = new ArrayList<>();
+        expectDaysOfSeptember.add(LocalDate.of(2022, 9, 5));
+        expectDaysOfSeptember.add(LocalDate.of(2022, 9, 6));
+        expectDaysOfSeptember.add(LocalDate.of(2022, 9, 7));
+        expectDaysOfSeptember.add(LocalDate.of(2022, 9, 1));
+        expectDaysOfSeptember.add(LocalDate.of(2022, 9, 2));
+        expectDaysOfSeptember.add(LocalDate.of(2022, 9, 3));
+        expectDaysOfSeptember.add(LocalDate.of(2022, 9, 4));
+
+        List<LocalDate> actualDaysOfSeptember = new ArrayList<>();
+        for (int i = 1; i <= 7; i++) {
+            actualDaysOfSeptember.add(holidayService.getDayOfMonth(LocalDate.of(2022, 9, 1), DayOfWeek.of(i), 1));
+        }
+
+        assertEquals(expectDaysOfSeptember, actualDaysOfSeptember);
+    }
+
     
     @Test
-    void getfirstMondayOfJanuary2022() {
-        
+    void testSecondWeekOfMonthSeptember2022() {
+        List<LocalDate> expectDaysOfSeptember = new ArrayList<>();
+        expectDaysOfSeptember.add(LocalDate.of(2022, 9, 12));
+        expectDaysOfSeptember.add(LocalDate.of(2022, 9, 13));
+        expectDaysOfSeptember.add(LocalDate.of(2022, 9, 14));
+        expectDaysOfSeptember.add(LocalDate.of(2022, 9, 8));
+        expectDaysOfSeptember.add(LocalDate.of(2022, 9, 9));
+        expectDaysOfSeptember.add(LocalDate.of(2022, 9, 10));
+        expectDaysOfSeptember.add(LocalDate.of(2022, 9, 11));
+
+        List<LocalDate> actualDaysOfSeptember = new ArrayList<>();
+        for (int i = 1; i <= 7; i++) {
+            actualDaysOfSeptember.add(holidayService.getDayOfMonth(LocalDate.of(2022, 9, 1), DayOfWeek.of(i), 2));
+        }
+
+        assertEquals(expectDaysOfSeptember, actualDaysOfSeptember);
     }
+    
+ @Test
+ void testLastWeekOfMonthSeptember2022() {
+     int monthNum = 9;
+     List<LocalDate> expectDays = new ArrayList<>();
+     expectDays.add(LocalDate.of(2022, monthNum, 25));
+     expectDays.add(LocalDate.of(2022, monthNum, 26));
+     expectDays.add(LocalDate.of(2022, monthNum, 27));
+     expectDays.add(LocalDate.of(2022, monthNum, 28));
+     expectDays.add(LocalDate.of(2022, monthNum, 29));
+     expectDays.add(LocalDate.of(2022, monthNum, 30));
+     expectDays.add(LocalDate.of(2022, monthNum, 24));
+
+     List<LocalDate> actualDays = new ArrayList<>();
+     for (int i = 1; i <= 7; i++) {
+         actualDays.add(holidayService.getDayOfMonth(LocalDate.of(2022, monthNum, 1), DayOfWeek.of(i), 5));
+     }
+
+     assertEquals(expectDays, actualDays);
+ }
+
+ @Test
+ void testLastWeekOfMonthOctober2022() {
+     int monthNum = 10;
+     List<LocalDate> expectDays = new ArrayList<>();
+     expectDays.add(LocalDate.of(2022, monthNum, 30));
+     expectDays.add(LocalDate.of(2022, monthNum, 31));
+     expectDays.add(LocalDate.of(2022, monthNum, 25));
+     expectDays.add(LocalDate.of(2022, monthNum, 26));
+     expectDays.add(LocalDate.of(2022, monthNum, 27));
+     expectDays.add(LocalDate.of(2022, monthNum, 28));
+     expectDays.add(LocalDate.of(2022, monthNum, 29));
+
+     List<LocalDate> actualDays = new ArrayList<>();
+     for (int i = 1; i <= 7; i++) {
+         actualDays.add(holidayService.getDayOfMonth(LocalDate.of(2022, monthNum, 1), DayOfWeek.of(i), 5));
+     }
+
+     assertEquals(expectDays, actualDays);
+ }
 }
